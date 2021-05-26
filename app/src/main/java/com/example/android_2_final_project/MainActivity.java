ae68da7d61aa1cce29d1601eb2fb2907a9af5c36
Alex <alex26g4@hotmail.com>
2021-05-26 16:19:18 +0300

    minor changes

--- app/src/main/java/com/example/android_2_final_project/MainActivity.java
+++ app/src/main/java/com/example/android_2_final_project/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.android_2_final_project.fragments.LoginPageFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction().add(R.id.root,new LoginPageFragment()).commit();
+
     }
 }
