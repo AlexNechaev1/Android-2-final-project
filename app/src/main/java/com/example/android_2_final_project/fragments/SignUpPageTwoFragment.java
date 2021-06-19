49fb30100bcf6f307acffa65c1951e46d289f878
Alex <alex26g4@hotmail.com>
2021-06-19 22:58:54 +0300

    SignUpPageTwoFragment.java

---
+++ app/src/main/java/com/example/android_2_final_project/fragments/SignUpPageTwoFragment.java
@@ -0,0 +1,48 @@
+package com.example.android_2_final_project.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.android_2_final_project.R;
+
+public class SignUpPageTwoFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_signup_page_two, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        //Navigation.findNavController(view).navigate(R.id.action_signupPageTwo_to_explorerFragment);
+
+
+        initViews(view);
+
+        setListeners();
+    }
+
+    private void initViews(View v) {
+
+    }
+
+    private void setListeners() {
+
+    }
+}
