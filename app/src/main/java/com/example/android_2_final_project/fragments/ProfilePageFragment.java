e5479a382ee760c023549faecc85d93c78ea7d9d
Alex <alex26g4@hotmail.com>
2021-06-25 13:58:43 +0300

    ProfilePageFragment.java

---
+++ app/src/main/java/com/example/android_2_final_project/fragments/ProfilePageFragment.java
@@ -0,0 +1,35 @@
+package com.example.android_2_final_project.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.android_2_final_project.R;
+
+public class ProfilePageFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile_page, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        initViews(view);
+        setListeners(view);
+
+    }
+
+    private void initViews(View view) {
+
+    }
+
+    private void setListeners(View view) {
+    }
+}
