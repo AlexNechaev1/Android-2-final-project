4719ea2e5cbc3168b7f9d8814061472a5489beef
Alex <alex26g4@hotmail.com>
2021-06-25 13:57:02 +0300

    Minor Changes

--- app/src/main/java/com/example/android_2_final_project/fragments/CarDetailsFragment.java
+++ app/src/main/java/com/example/android_2_final_project/fragments/CarDetailsFragment.java
@@ -17,6 +17,10 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+
 import com.bumptech.glide.Glide;
 import com.example.android_2_final_project.R;
 import com.example.android_2_final_project.models.Car;
@@ -72,7 +76,7 @@ public class CarDetailsFragment extends Fragment {
         mSeeSellerProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO: transition to seller profile
+                Navigation.findNavController(v).navigate(R.id.action_carDetailsFragment_to_profilePageFragment);
             }
         });
 
@@ -129,7 +133,9 @@ public class CarDetailsFragment extends Fragment {
 
         switch (item.getItemId()){
             case R.id.action_go_to_seller:
-                // navigate to seller profile
+                NavHostFragment navHostFragment = (NavHostFragment) requireActivity().getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+                NavController navController = navHostFragment.getNavController();
+                navController.navigate(R.id.action_carDetailsFragment_to_profilePageFragment);
                 break;
             case R.id.action_follow:
                 // add current post to user's saved posts.
