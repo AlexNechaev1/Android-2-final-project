31c7a0f4af0787ba4334d389764abeed92b0565e
ManchiGitHub <66803951+ManchiGitHub@users.noreply.github.com>
2021-06-30 00:35:39 +0300

    chat integration continue

---
+++ app/src/main/java/com/example/android_2_final_project/repository/ChatRepository.java
@@ -0,0 +1,14 @@
+package com.example.android_2_final_project.repository;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Singleton
+public class ChatRepository {
+
+
+
+    @Inject
+    public ChatRepository() {
+    }
+}
