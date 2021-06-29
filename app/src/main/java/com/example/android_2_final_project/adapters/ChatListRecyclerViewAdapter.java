b516bb255f9c81542aaf354ac2607d8cfa921c7f
Alex <alex26g4@hotmail.com>
2021-06-30 01:12:24 +0300

    center crop and round corners

--- app/src/main/java/com/example/android_2_final_project/adapters/ChatListRecyclerViewAdapter.java
+++ app/src/main/java/com/example/android_2_final_project/adapters/ChatListRecyclerViewAdapter.java
@@ -12,6 +12,9 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.android_2_final_project.R;
 import com.example.android_2_final_project.models.ChatModel;
 import com.example.android_2_final_project.models.UserModel;
@@ -61,6 +64,7 @@ public class ChatListRecyclerViewAdapter extends FirebaseRecyclerAdapter<String,
                     UserModel user = Objects.requireNonNull(task.getResult()).getValue(UserModel.class);
                     Glide.with(mContext)
                             .load(Objects.requireNonNull(user).getProfileImage())
+                            .apply(new RequestOptions().transform(new CenterCrop(), new RoundedCorners(50)))
                             .into(holder.profileIv);
 
                     holder.contactName.setText(user.getUsername());
